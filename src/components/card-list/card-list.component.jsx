8da1b0456a03a0672d66c2cb281724c9fe598522
Your Name <you@example.com>
2020-12-16 18:24:24 +1100

    Added phase1

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './card-list.styles.css'
+import './card-list.styles.css';
+import {Card} from '../card/card.component';
 
 export const CardList = (props)=>{
     return <div className="card-list">    
     
     {props.monsters.map((monster) => {
-        return <h1 key={monster.id}>{monster.name}</h1>;
+        return <Card id={monster.id} monster={monster}></Card>;
       })}
 
     </div>
